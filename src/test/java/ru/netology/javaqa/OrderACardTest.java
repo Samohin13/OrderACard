78fb2fa140b0ffc2a0bc551598719ac2a685c792
Samohin Sergei <Bravochief21@gmail.com>
2024-09-17 18:41:49 +0500

    shouldCardOrderTEST_NEW

--- src/test/java/ru/netology/javaqa/OrderACardTest.java
+++ src/test/java/ru/netology/javaqa/OrderACardTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -50,18 +49,17 @@ public class OrderACardTest {
         // загрузить страницу
         driver.get("http://localhost:9999");
         // Поиск элементов
-        WebElement name = driver.findElement(By.cssSelector("[data-test-id='name'].input"));
-        WebElement phone = driver.findElement(By.cssSelector("[data-test-id='phone'].input"));
-        WebElement checkbox = driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box"));
-        WebElement button = driver.findElement(By.cssSelector("[data-test-id='submit'] .button"));
-
+        WebElement nameField = driver.findElement(By.cssSelector("[data-test-id=name] input"));
+        WebElement phoneField = driver.findElement(By.cssSelector("[data-test-id='phone'] input"));
+        WebElement checkbox = driver.findElement(By.cssSelector(".checkbox__box"));//
+        WebElement submitButton = driver.findElement(By.cssSelector(".button"));
         // Данные
-        name.sendKeys("Самохин Сергей");
-        phone.sendKeys("+79890000000");
+        nameField.sendKeys("Самохин Сергей");
+        phoneField.sendKeys("+79890000000");
         checkbox.click();
-        button.click();
+        submitButton.click();
 
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+       String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
 
         // Сравниение
